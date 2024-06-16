43c1f9c4131fbfcf4162e4f7ad140c82e43cda9f
Your GitHub Usernamemsdubova <femme777@gmail.com>
2024-06-16 14:30:30 +0100

    Created server 8080

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
-func main () {
-	
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	plans := PlanResource{}
+	mux.HandleFunc("GET /plans", plans.GetAll)
+
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		fmt.Println("Невдала спроба створити та прослухати 8080", err)
+	}
+}
+
+type PlanResource struct {
+}
+
+func (p *PlanResource) GetAll(w http.ResponseWriter, r http.Request) {
+
 }
